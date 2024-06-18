7c8d649543f06349da04fe9ec944ca81817ac663
dipto <moniruzzamandipto5@gmail.com>
2024-06-18 14:38:45 +0600

    Completed footer

--- components/Layout/Client/footer.js
+++ components/Layout/Client/footer.js
@@ -24,42 +24,92 @@ const Footer = (props) => {
   const settings = useSelector((state) => state.settings);
   const { t } = useTranslation();
   const footer_link = {
-    company: [
+    CustomerService: [
       {
-        name: t("about_us"),
-        link: "/about",
+        name: t("Follow your order/return"),
+        link: "/order-track",
       },
-    ],
-    shop: [
       {
         name: t("faq"),
         link: "/faq",
       },
       {
-        name: t("privacy_policy"),
-        link: "/privacy",
-      },
-      {
-        name: t("terms_and_conditions"),
-        link: "/terms",
+        name: t("orders"),
+        link: "/profile?tab=0",
       },
       {
         name: t("return_policy"),
         link: "/return",
       },
+      {
+        name: t("Contact us"),
+        link: "/contact_us",
+      },
     ],
-    account: [
+    ProductGuide: [
       {
-        name: t("signin"),
-        link: "/signin",
+        name: t("Size guide"),
+        link: "/product-guide-size",
       },
       {
-        name: t("profile"),
-        link: "/profile",
+        name: t("Style guide"),
+        link: "/product-style-size",
       },
       {
-        name: t("track_order"),
-        link: "/order-track",
+        name: t("Summer style guide"),
+        link: "/summer-style-guide",
+      },
+      {
+        name: t("Fabrics"),
+        link: "/fabrics",
+      },
+      {
+        name: t("Patterned"),
+        link: "/patterned",
+      },
+      {
+        name: t("Care"),
+        link: "/care",
+      },
+      {
+        name: t("Gift card"),
+        link: "/gift-card",
+      },
+    ],
+    AboutUs: [
+      {
+        name: t("Company"),
+        link: "/about",
+      },
+      {
+        name: t("Careers"),
+        link: "/careers",
+      },
+      {
+        name: t("Store locations"),
+        link: "/store-locations",
+      },
+      {
+        name: t("Sales"),
+        link: "/sales",
+      },
+      {
+        name: t("Promotions"),
+        link: "/promotions",
+      },
+    ],
+    LegalArea: [
+      {
+        name: t("privacy_policy"),
+        link: "/privacy",
+      },
+      {
+        name: t("Cookie policy"),
+        link: "/cookie-policy",
+      },
+      {
+        name: t("terms_and_conditions"),
+        link: "/terms",
       },
     ],
   };
@@ -68,7 +118,7 @@ const Footer = (props) => {
     return (
       <>
         <footer className={classes.footer_container}>
-          <div className="custom_container">
+          <div className="px-xl-5 px-md-2 px-2">
             <div className="row m-0">
               <div className="col-md-4 px-0 py-4">
                 <div className={classes.icon_container}>
@@ -124,7 +174,21 @@ const Footer = (props) => {
                   <h1>{settings.settingsData.description}</h1>
                 </div>
               </div>
-              <div className="col-md-3 px-3 py-2">
+              <div className="col-md-9">
+                <div className="row">
+                  {Object.entries(footer_link).map((x, i) => (
+                    <div className="col-md-3 px-3 py-2" key={i}>
+                      <h3 className={classes.footer_heading}>{t(x[0])}</h3>
+                      <ul className={classes.list}>
+                        {x[1]?.map((link) => (
+                          <li className={classes.list_item} key={link.name}>
+                            <Link href={link.link}>{link.name}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                  {/* <div className="col-md-3 px-3 py-2">
                 <h3 className={classes.footer_heading}>{t("contact_info")}</h3>
                 <div className={classes.address}>
                   <div>
@@ -170,8 +234,11 @@ const Footer = (props) => {
                     </li>
                   ))}
                 </ul>
+              </div> */}
+                </div>
               </div>
             </div>
+
             <hr />
             <div className="row m-0">
               <div className="col-md-3 p-2">
